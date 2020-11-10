de87bae4e77cd291d19925a41b7981bd9846fe56
Raveen K Sharma <raveen08@gmail.com>
2020-11-10 22:24:32 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,8 +10,8 @@ def show
   def create
     byebug
     @student = Student.new
-    @student.first_name = params[:first_name]
-    @student.last_name = params[:last_name]
+    @student.first_name = params[:student][:first_name]
+    @student.last_name = params[:student][:last_name]
     @student.save
     redirect_to student_path(@student)
   end
